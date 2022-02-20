9a82aef9d750f5c50602eff6bc829d5f5da81229
NickKhachtryan <nickoarm@mail.ru>
2022-02-20 14:35:47 +0300

    added functionality of buttons

--- ConcentrationApp/ViewController.swift
+++ ConcentrationApp/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var flipCount = 0{
+        didSet{
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    var emojiChoices = [ "🎃", "👻", "🎃", "👻"]
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+//  MARK: - ACTIONS
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender){
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("chosen card was not in var cardButtons")
+        }
+    }
+    
+    
+    //  MARK: - FUNCTIONS
+    
+    func flipCard(withEmoji emoji : String , on button: UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = UIColor.orange
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = UIColor.white
+        }
+    }
+    
 }
 
