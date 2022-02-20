c95189cef94b416c97d8f8f39951e04e3ccace3c
NickKhachtryan <nickoarm@mail.ru>
2022-02-20 14:41:39 +0300

    added concentrationModel (to be continued)

---
+++ ConcentrationApp/ConcentrationModel.swift
@@ -0,0 +1,18 @@
+//
+//  ConcentrationModel.swift
+//  ConcentrationApp
+//
+//  Created by Nick Khachatryan on 20.02.2022.
+//
+
+import Foundation
+
+class ConcentrationModel{
+    
+    var cards: Array<Card>
+    
+    func chooseCard(at index: Int){
+        
+    }
+    
+}
